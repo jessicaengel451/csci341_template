e4e761436bb603f0d695f339c4e024d047709bb8
jessicaengel451 <72846465+jessicaengel451@users.noreply.github.com>
2022-10-07 11:36:13 -0600

    add example helloworld, my resource, and the blank rsp that is used every week

---
+++ examples/helloworld.asm
@@ -0,0 +1,14 @@
+# Taken from: https://gist.github.com/argoc/77ffe85738c12d4bf178ccdb935d1aae
+
+
+  .data         # declare and initialize variables
+hello:  .asciz "Hello world!" # string with null terminator
+ 
+  .text         # code starts here
+main:           # label marking the entry point of the program
+  la a0, hello  # load the address of hello into $a0 (1st argument)
+  addi a7, zero, 4     # code to print the string at the address a0
+  ecall         # make the system call
+ 
+  addi a7, zero, 10    # code to exit
+  ecall         # make the system call
